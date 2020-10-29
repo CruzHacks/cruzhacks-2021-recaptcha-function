0b2cff898e4a65f0b942aae039787eeaf90d3593
Isai Lopez <ilopezro@ucsc.edu>
2020-10-28 18:04:27 -0700

    deleted comments

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,8 @@
 const functions = require('firebase-functions');
 const fetch = require("node-fetch")
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
-const client = new SecretManagerServiceClient();
-
-
-/**
- * TODO(developer): Uncomment these variables before running the sample.
- */
-// parent = 'projects/my-project', // Project for which to manage secrets.
-// secretId = 'foo', // Secret ID.
-// payload = 'hello world!' // String source data.
 
 exports.siteVerify = functions.https.onRequest(async (request, response) => {
 
-    
     const [accessResponse] = await client.accessSecretVersion({
         name: "projects/cruzhacks-4a899/secrets/RecaptchaSecretKey/versions/latest",
     });
