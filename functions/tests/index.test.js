c9307f342d04a60e8c34f14a69f53607b629f43e
ilopezro <ilopezro@ucsc.edu>
2020-10-28 21:56:06 -0700

    init mocha testing?

--- functions/tests/index.test.js
+++ functions/tests/index.test.js
@@ -1,40 +1,59 @@
-const context = require("./defaultContexts");
-const httpFunction = require("../index.js");
-
-describe("unit tests for index.js driver", () => {
-  describe("test recaptcha api", () => {
-    test("should return 400 when the token is missing", async () => {
-      const request = {
-        headers: {},
-      };
-      const response = {
-        status: function (status) {
-          expect(status).toEqual(400);
-          return this;
-        },
-        send: function (message) {
-          expect(message).toEqual("Token is missing");
-        },
-      };
-      await httpFunction.siteVerify(request, response);
-    });
-
-    test("should return 400 when the token is invalid", async () => {
-      const request = {
-        headers: {
-          token: "invalidtoken",
-        },
-      };
-      const response = {
-        status: function (status) {
-          expect(status).toEqual(400);
-          return this;
-        },
-        send: function (message) {
-          expect(message).toEqual("Invalid token provided");
-        },
-      };
-      await httpFunction.siteVerify(request, response);
-    });
-  });
+const chai = require("chai");
+const siteVerify = require("../index.js");
+const test = require("firebase-functions-test")();
+test.mockConfig({
+  siteverify: { recaptchasecretkey: "token", siteverifyurl: "url" },
 });
+
+// describe("unit tests for index.js driver", () => {
+//   describe("test recaptcha api", () => {
+//     test("should return 400 when the token is missing", async () => {
+//       const request = {
+//         headers: {},
+//       };
+//       const response = {
+//         status: function (status) {
+//           expect(status).toEqual(400);
+//           return this;
+//         },
+//         send: function (message) {
+//           expect(message).toEqual("Token is missing");
+//         },
+//       };
+//       await httpFunction.siteVerify(request, response);
+//     });
+
+//     test("should return 400 when the token is invalid", async () => {
+//       const request = {
+//         headers: {
+//           token: "invalidtoken",
+//         },
+//       };
+//       const response = {
+//         status: function (status) {
+//           expect(status).toEqual(400);
+//           return this;
+//         },
+//         send: function (message) {
+//           expect(message).toEqual("Invalid token provided");
+//         },
+//       };
+//       await httpFunction.siteVerify(request, response);
+//     });
+//   });
+// });
+const request = {
+  headers: {},
+};
+const res = {
+  status: (status) => {
+    chai.assert.equal(status, 401);
+  },
+  send: ({ error, status, message }) => {
+    chai.assert.equal(error, true);
+    chai.assert.equal(status, 401);
+    chai.assert.equal(message, "Token is missing");
+  },
+};
+
+siteVerify.siteVerify(request, res);
