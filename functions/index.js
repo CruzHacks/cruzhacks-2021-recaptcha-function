b3585226886ccc4140f642821de585930877547d
PriyaRajarathinam <35835697+PriyaRajarathinam@users.noreply.github.com>
2020-10-29 20:21:58 -0700

    Recaptcha function (#1)
    * added siteVerify function
    
    * Updated function to take in header values
    
    * deleted comments
    
    * added functions env vars
    
    * updated dependencies
    
    * got rid completely of all potentially vulnerable variables
    
    * added linting
    
    * added appropriate responses
    
    * returned the function
    
    * added end lines
    
    * init mocha testing?
    
    * fixed package json and workflow
    
    * Added test cases
    
    * added test case for 200
    
    * deleted second describe block, added a test, deleted unnecessary comment
    
    * changed testing suite name
    
    Co-authored-by: Priya Rajarathinam <cat@jjjjjjj.attlocal.net>
    Co-authored-by: Isai Lopez <ilopezro@ucsc.edu>

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,65 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+require("isomorphic-fetch");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.siteVerify = functions.https.onRequest(async (request, response) => {
+  const secretKey = functions.config().siteverify.recaptchasecretkey;
+  const siteVerifyUrl = functions.config().siteverify.siteverifyurl;
+  if (!Object.keys(request.headers).includes("token")) {
+    return response
+      .status(401)
+      .send({ error: true, status: 401, message: "Token is missing" });
+  }
+  const token = request.headers.token;
+  const formBody = `secret=${secretKey}&response=${token}`;
+  return fetch(siteVerifyUrl, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    method: "POST",
+    body: formBody,
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      if (Object.keys(json).includes("error-codes")) {
+        const errors = json["error-codes"];
+        if (
+          errors.includes("invalid-input-response") &&
+          errors.includes("invalid-input-secret")
+        ) {
+          return response.status(400).send({
+            error: true,
+            status: 400,
+            message: "Invalid Token and Secret Provided",
+          });
+        } else if (errors.includes("invalid-input-response")) {
+          return response.status(400).send({
+            error: true,
+            status: 400,
+            message: "Invalid Token Provided",
+          });
+        } else if (errors.includes("invalid-input-secret")) {
+          return response.status(401).send({
+            error: true,
+            status: 401,
+            message: "Incorrect Secret Provided",
+          });
+        } else if (errors.includes("timeout-or-duplicate")) {
+          return response.status(400).send({
+            error: true,
+            status: 400,
+            message: "Request Timed Out or Sent Duplicate Key",
+          });
+        }
+      }
+      return response.status(200).send({
+        error: false,
+        status: 200,
+        message: "Succesfully Authenticated Request",
+      });
+    })
+    .catch((error) => {
+      return response
+        .status(500)
+        .send({ error: true, status: 500, message: error.message });
+    });
+});
--- functions/tests/index.test.js
+++ functions/tests/index.test.js
@@ -1,4 +1,154 @@
-const context = require("./defaultContexts");
+const chai = require("chai");
+const siteVerify = require("../index.js");
+const fetchMock = require("fetch-mock");
+const test = require("firebase-functions-test")();
+test.mockConfig({
+  siteverify: {
+    recaptchasecretkey: "token",
+    siteverifyurl: "http://www.test.com/siteVerify",
+  },
+});
+describe("siteVerify Testing Suite", () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
 
-test.skip("Write tests in here", () => {
-})
+  it("return 401 when token is missing", (done) => {
+    const req = {
+      headers: {},
+    };
+    const res = {
+      status: (status) => {
+        chai.assert.equal(status, 401);
+        return res;
+      },
+      send: ({ error, status, message }) => {
+        chai.assert.equal(error, true);
+        chai.assert.equal(status, 401);
+        chai.assert.equal(message, "Token is missing");
+        done();
+      },
+    };
+    siteVerify.siteVerify(req, res);
+  });
+
+  it("return 400 when the token is invalid", (done) => {
+    fetchMock.mock("http://www.test.com/siteVerify", {
+      "error-codes": ["invalid-input-response"],
+    });
+    const req = {
+      headers: {
+        token: "token",
+      },
+    };
+    const res = {
+      status: (status) => {
+        chai.assert.equal(status, 400);
+        return res;
+      },
+      send: ({ error, status, message }) => {
+        chai.assert.equal(error, true);
+        chai.assert.equal(status, 400);
+        chai.assert.equal(message, "Invalid Token Provided");
+        done();
+      },
+    };
+    siteVerify.siteVerify(req, res);
+  });
+
+  it("return 400 when invalid secret and token is provided", (done) => {
+    fetchMock.mock("http://www.test.com/siteVerify", {
+      "error-codes": ["invalid-input-response", "invalid-input-secret"],
+    });
+    const req = {
+      headers: {
+        token: "token",
+      },
+    };
+    const res = {
+      status: (status) => {
+        chai.assert.equal(status, 400);
+        return res;
+      },
+      send: ({ error, status, message }) => {
+        chai.assert.equal(error, true);
+        chai.assert.equal(status, 400);
+        chai.assert.equal(message, "Invalid Token and Secret Provided");
+        done();
+      },
+    };
+    siteVerify.siteVerify(req, res);
+  });
+
+  it("return 401 when invalid secret is provided", (done) => {
+    fetchMock.mock("http://www.test.com/siteVerify", {
+      "error-codes": ["invalid-input-secret"],
+    });
+    const req = {
+      headers: {
+        token: "token",
+      },
+    };
+    const res = {
+      status: (status) => {
+        chai.assert.equal(status, 401);
+        return res;
+      },
+      send: ({ error, status, message }) => {
+        chai.assert.equal(error, true);
+        chai.assert.equal(status, 401);
+        chai.assert.equal(message, "Incorrect Secret Provided");
+        done();
+      },
+    };
+    siteVerify.siteVerify(req, res);
+  });
+
+  it("return 400 when duplicate key sent", (done) => {
+    fetchMock.mock("http://www.test.com/siteVerify", {
+      "error-codes": ["timeout-or-duplicate"],
+    });
+    const req = {
+      headers: {
+        token: "token",
+      },
+    };
+    const res = {
+      status: (status) => {
+        chai.assert.equal(status, 400);
+        return res;
+      },
+      send: ({ error, status, message }) => {
+        chai.assert.equal(error, true);
+        chai.assert.equal(status, 400);
+        chai.assert.equal(message, "Request Timed Out or Sent Duplicate Key");
+        done();
+      },
+    };
+    siteVerify.siteVerify(req, res);
+  });
+
+  it("return 200 if request was successful", (done) => {
+    fetchMock.mock("http://www.test.com/siteVerify", {});
+    const req = {
+      headers: {
+        token: "token",
+      },
+    };
+    const res = {
+      status: (status) => {
+        chai.assert.equal(status, 200);
+        return res;
+      },
+      send: ({ error, status, message }) => {
+        chai.assert.equal(error, false);
+        chai.assert.equal(status, 200);
+        chai.assert.equal(message, "Succesfully Authenticated Request");
+        done();
+      },
+    };
+    siteVerify.siteVerify(req, res);
+  });
+});
+
+fetchMock.reset();
